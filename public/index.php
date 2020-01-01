c1915d3ec6f82289d93e99d5bad1fe5c1d77ce1d
Olaleye Obidiya <olaleyeobidiya@gmail.com>
2020-01-01 18:18:02 +0100

    Tests the database connection

--- public/index.php
+++ public/index.php
@@ -1,3 +1,8 @@
+<?php
+
+    require_once("../resources/config.php");
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
---
+++ resources/functions.php
@@ -0,0 +1,8 @@
+<?php
+
+    if($connection)
+    {
+        echo "Connected from functions.php";
+    }
+
+?>
